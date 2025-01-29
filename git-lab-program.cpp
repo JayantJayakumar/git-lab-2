94a7af573dcdf874b9850b8afa9bb7b6dcec334e
JayantJayakumar <jj483124@ohio.edu>
2025-01-29 10:28:01 -0500

    Commit git-lab-program.cpp

---
+++ git-lab-program.cpp
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jayant Jayakumar
+ *        Date: 01/29/25
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
